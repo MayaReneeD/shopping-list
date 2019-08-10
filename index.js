83f68e688394d8dd4d2bbbf88f226b66fb7ac69e
Maya Debaroncelli <mdebaroncelli@gmail.com>
2019-08-10 00:04:38 -0700

    add

--- index.js
+++ index.js
@@ -1,36 +1,31 @@
-$(document).ready (function(){
-    //hearing for the submit button
-    $('#js-shopping-list-form').submit(function(event){
-    //preventing the browser from refreshing
+$(function(){
+  $('form').submit(function(event){
     event.preventDefault();
-    //define variable to add new item to list 
-    const newItem = $('.js-shopping-list-entry').val();
-    //allow user the ability to clear inputs
-    $('.js-shopping-list-entry').val('');
-    //allow to the user to add the new item to list
-    $('.shopping-list').append(
-      `<li>
-          <span class="shopping-item">${newItem}</span>
-          <div class="shopping-item-controls">
-            <button class="shopping-item-toggle">
-              <span class="button-label">check</span>
-            </button>
-            <button class="shopping-item-delete">
-              <span class="button-label">delete</span>
-            </button>
-          </div>
-        </li>`);
-    });
-    
-    //need to delete item when user clicks on delete button
-    $('.shopping-list').on('click','.shopping-item-delete', function(event){
-      $(this).closest('li').remove();
-    });
-    //need to find item and check off item 
-    $('.shopping-list').on('click','.shopping-item-toggle',function(event){
-      $(this).closet('li').find('.shopping-item').toggleClass('.shopping-item_checked');
-    });
-    
+    let item = $('#shopping-list-entry').val();
+    $('ul').append(`<li>
+        <span class="shopping-item">${item}</span>
+        <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+        </button>
+        </div>
+    </li>`)
+
+  })  
+  $('ul').on('click','.shopping-item-toggle', function(event){
+    $(this).closest('li')
+    .find('shopping-item')
+    .toggleClass
+    ('.shopping-item_checked');
   
-    
-  });
+  })
+
+  $('ul').on('click','.shopping-item-delete', function(event){
+      $(this).closest('li')
+      .remove();
+})
+
+});
